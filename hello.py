3ce015c99a25e07f3ee40ea5361a65e1fba7205b
NKRifat47 <nk.rifat47@gmail.com>
2026-04-15 17:55:33 +0600

    second commit

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
 print("Hello, World!")
 
-print("Welcome to Python programming! this is a simple program to learn jenkins and git")
+print("Welcome to Python programming!")
+
+print("This is a simple program to learn jenkins and git")
